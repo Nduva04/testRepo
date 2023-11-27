4306d2135c968ce09a545d07d9275a64981f7a5b
Nduva04 <nduva1742@students.mc3.edu>
2023-11-27 12:32:22 -0500

    Java file test trial
    File prints hello world, then prints numbers 1-100 in a for loop.

---
+++ testfilejava.java
@@ -0,0 +1,12 @@
+public class testfilejava
+{
+   public static void main(String[] args)
+   {
+      System.out.println("hello world");
+      
+      for (int i = 1; i <= 100; i++)
+      {
+         System.out.println(i);
+      }        
+   }
+}
